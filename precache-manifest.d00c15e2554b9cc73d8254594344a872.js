6bfc2e5ad20310eb5f82a631dd3a1d328bfe21d5
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-12-09 10:28:42 +0100

    Updates

---
+++ precache-manifest.d00c15e2554b9cc73d8254594344a872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d167d1aa7ae4571185a00a8b3f46b2",
+    "url": "/ytdj/index.html"
+  },
+  {
+    "revision": "ca5b2b05c378a4404f4e",
+    "url": "/ytdj/static/css/main.e4893cb2.chunk.css"
+  },
+  {
+    "revision": "a045ea986cd7e339d7c4",
+    "url": "/ytdj/static/js/2.2a893577.chunk.js"
+  },
+  {
+    "revision": "ca5b2b05c378a4404f4e",
+    "url": "/ytdj/static/js/main.2eb50fc3.chunk.js"
+  },
+  {
+    "revision": "36a1121df196a6bab717",
+    "url": "/ytdj/static/js/runtime-main.557b8caf.js"
+  }
+]);
