d8b9eee6f6b16ba7c8ea1c5690ec9daab5cef313
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-12-09 17:08:01 +0100

    fix missing file

---
+++ src/components/videoItem/VideoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './videoItem.scss';
+
+const VideoItem = ({ video, handleVideoSelect }) => {
+    return (
+        <div onClick={() => handleVideoSelect(video)} className='item__container'>
+            <div className='item__image__container'>
+                <img src={video.snippet.thumbnails.default.url} alt='item searched' />
+            </div>
+            <div className='item__title'>
+               <p>{video.snippet.title}</p>
+            </div>
+        </div>
+    )
+}
+
+export default VideoItem;
